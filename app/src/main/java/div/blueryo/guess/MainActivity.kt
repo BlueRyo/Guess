49690b0a9003645ce9706848e09ea072efd1a282
BlueRyo <blueryo@gmail.com>
2019-04-25 21:26:05 +0800

    function design, toast, dialog

--- app/src/main/java/div/blueryo/guess/MainActivity.kt
+++ app/src/main/java/div/blueryo/guess/MainActivity.kt
@@ -2,11 +2,41 @@ package div.blueryo.guess
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val secretNumber=SecretNumber()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        findViewById<TextView>(R.id.txtView_Hello).text=secretNumber.secret.toString()
+        Log.d("MainActivity","secret=${secretNumber.secret}")
+    }
+
+
+    fun chkNum(view: View){
+        val num=ed_number.text.toString().toInt()
+        Log.d("MainActivity","number:$num")
+        var diff=secretNumber.validate(num)
+        var msg="恭禧你答對了"
+        if (diff < 0 ){
+            msg="再大一點"
+        } else {
+            if (diff>0){
+                msg="再小一點"
+            }
+        }
+//        Toast.makeText(this,msg,Toast.LENGTH_LONG)
+        AlertDialog.Builder(this)
+            .setMessage(msg)
+            .setTitle("訊息")
+            .setPositiveButton("ok",null)
+            .show()
     }
 }
